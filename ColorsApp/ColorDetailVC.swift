e84162f9bdc7c4fc8c759557c4d63bebb43a80ae
medfarouk <mohamed.farouk@lepotcommun.fr>
2024-03-18 01:12:49 +0100

    Initialize the project Add navigation controller add ColorDetail ViewController

---
+++ ColorsApp/ColorDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ColorDetailVC.swift
+//  ColorsApp
+//
+//  Created by Mohamed Farouk Ben Akacha on 18/3/2024.
+//
+
+import UIKit
+
+class ColorDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
